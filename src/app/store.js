61d4db8a03de2a46b64959b9baf8825ea262147e
rajibdas-it <rajibrad@gmail.com>
2023-03-20 16:12:59 +0600

    production level e jeno redux devtools e data na dekha jay seta thik korar jonno

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import apiSlice from "../features/api/apiSlice";
+import authSlice from "../features/auth/authSlice";
+import conversationSlice from "../features/conversations/conversationSlice";
+import messagesSlice from "../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSlice,
+    conversations: conversationSlice,
+    messages: messagesSlice,
   },
   middleware: (getDefaultMiddlewares) =>
     getDefaultMiddlewares().concat(apiSlice.middleware),
+  devTools: process.env.NODE_ENV !== "production",
 });
