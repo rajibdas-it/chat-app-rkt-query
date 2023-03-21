569935509d54c49473ab59f05c52a433ba860a8d
rajibdas-it <rajibrad@gmail.com>
2023-03-21 17:30:40 +0600

    work now

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,17 +1,20 @@
 import { useSelector } from "react-redux";
+import gravatarUrl from "gravatar-url";
 
-export default function ChatHead(message) {
-  //   console.log(message);
-  const { sender, receiver } = message || {};
-  const { user } = useSelector((state) => state.auth);
+export default function ChatHead({ message }) {
+  const { user } = useSelector((state) => state.auth) || {};
   const { email } = user || {};
-
+  const { sender, receiver } = message || {};
   const partnerEmail =
     sender?.email === email ? receiver?.email : sender?.email;
   const partnerName = sender?.email === email ? receiver?.name : sender?.name;
   return (
     <div className="relative flex items-center p-3 border-b border-gray-300">
-      <img className="object-cover w-10 h-10 rounded-full" src="" alt="" />
+      <img
+        className="object-cover w-10 h-10 rounded-full"
+        src={gravatarUrl(partnerEmail)}
+        alt={partnerName}
+      />
       <span className="block ml-2 font-bold text-gray-600">{partnerName}</span>
     </div>
   );
