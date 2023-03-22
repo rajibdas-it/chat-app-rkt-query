e615f02382b6b74e8ad907e044f3c6fa8994bd04
rajibdas-it <rajibrad@gmail.com>
2023-03-22 22:02:08 +0600

    everything works fine

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -24,7 +24,7 @@ export default function ChatBody() {
       <>
         <ChatHead message={messages[0]} />
         <Messages messages={messages} />
-        <Options />
+        <Options info={messages[0]} />
       </>
     );
   }
